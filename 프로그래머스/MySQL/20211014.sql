243271a770212a71aadddeee6ce24d75aa3f6a53
Brandon Jung <jwcnet93@gmail.com>
2021-10-14 12:47:43 +0900

    Create 20211014.sql

---
+++ 프로그래머스/MySQL/20211014.sql
@@ -0,0 +1,35 @@
+-- 상위 n개 레코드 문제
+SELECT NAME FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
+-----------------------------------------------
+-- 최댓값 구하기 문제
+SELECT DATETIME FROM ANIMAL_INS
+ORDER BY DATETIME DESC
+LIMIT 1;
+-----------------------------------------------
+-- 최솟값 구하기 문제
+SELECT DATETIME FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
+-----------------------------------------------
+-- 동물 수 구하기 문제
+SELECT COUNT(ANIMAL_ID) FROM ANIMAL_INS
+-----------------------------------------------
+-- 중복 제거하기 문제
+SELECT COUNT(DISTINCT NAME)
+FROM ANIMAL_INS;
+-----------------------------------------------
+-- 고양이와 개는 몇 마리 있을까 문제
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) AS count 
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE;
+-----------------------------------------------
+-- 동명 동물 수 찾기
+SELECT NAME, COUNT(NAME) AS COUNT
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL -- NULL값이 아닌 것들만...
+GROUP BY NAME
+HAVING COUNT > 1 -- 두 번 이상 쓰인 이름과 해당 이름이 쓰인 횟수를 조회하는 것이므로
+ORDER BY NAME;
