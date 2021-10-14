d9b662610ab4d5c2460ba38a5e70a8d4f4db5399
Brandon Jung <jwcnet93@gmail.com>
2021-10-14 14:12:32 +0900

    Update 20211014_Lv1.sql

--- 프로그래머스/MySQL/20211014_Lv1.sql
+++ 프로그래머스/MySQL/20211014_Lv1.sql
@@ -1,21 +1,42 @@
--- 상위 n개 레코드 (Level 1)
-SELECT NAME FROM ANIMAL_INS
-ORDER BY DATETIME
-LIMIT 1;
+-- 모든 레코드 조회하기 (SELECT)
+SELECT * FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
 -----------------------------------------------
--- 최댓값 구하기 (Level 1)
+-- 최댓값 구하기 (SUM, MAX, MIN)
 SELECT DATETIME FROM ANIMAL_INS
 ORDER BY DATETIME DESC
 LIMIT 1;
 -----------------------------------------------
--- 모든 레코드 조회하기 (Level 1)
-SELECT * FROM ANIMAL_INS
-ORDER BY ANIMAL_ID;
------------------------------------------------
--- 이름이 없는 동물의 아이디
+-- 이름이 없는 동물의 아이디 (IS NULL)
 SELECT ANIMAL_ID FROM ANIMAL_INS
 WHERE NAME IS NULL;
 -----------------------------------------------
--- 이름이 있는 동물의 아이디
+-- 역순 정렬하기 (SELECT)
+SELECT NAME, DATETIME FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC;
+-----------------------------------------------
+-- 이름이 있는 동물의 아이디 (IS NULL)
 SELECT ANIMAL_ID FROM ANIMAL_INS
 WHERE NAME IS NOT NULL;
+-----------------------------------------------
+-- 아픈 동물 찾기 (SELECT)
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick';
+-----------------------------------------------
+-- 어린 동물 찾기 (SELECT)
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
+WHERE INTAKE_CONDITION != 'Aged'
+ORDER BY ANIMAL_ID;
+-----------------------------------------------
+-- 동물의 아이디와 이름 (SELECT)
+SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+-----------------------------------------------
+-- 여러 기준으로 정렬하기 (SELECT)
+SELECT ANIMAL_ID, NAME, DATETIME FROM ANIMAL_INS
+ORDER BY NAME, DATETIME DESC;
+-----------------------------------------------
+-- 상위 n개 레코드 (SELECT)
+SELECT NAME FROM ANIMAL_INS
+ORDER BY DATETIME
+LIMIT 1;
