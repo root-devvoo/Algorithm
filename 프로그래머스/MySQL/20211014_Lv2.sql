49f6017a4284528719c59a6514401c4d3d92bd0e
Brandon Jung <jwcnet93@gmail.com>
2021-10-14 13:03:46 +0900

    Update and rename 20211014.sql to 20211014_Lv2.sql

--- 프로그래머스/MySQL/20211014_Lv2.sql
+++ 프로그래머스/MySQL/20211014_Lv2.sql
@@ -1,32 +1,22 @@
--- 상위 n개 레코드
-SELECT NAME FROM ANIMAL_INS
-ORDER BY DATETIME
-LIMIT 1;
------------------------------------------------
--- 최댓값 구하기
-SELECT DATETIME FROM ANIMAL_INS
-ORDER BY DATETIME DESC
-LIMIT 1;
------------------------------------------------
--- 최솟값 구하기
+-- 최솟값 구하기 (Level 2)
 SELECT DATETIME FROM ANIMAL_INS
 ORDER BY DATETIME
 LIMIT 1;
 -----------------------------------------------
--- 동물 수 구하기
+-- 동물 수 구하기 (Level 2)
 SELECT COUNT(ANIMAL_ID) FROM ANIMAL_INS
 -----------------------------------------------
--- 중복 제거하기
+-- 중복 제거하기 (Level 2)
 SELECT COUNT(DISTINCT NAME)
 FROM ANIMAL_INS;
 -----------------------------------------------
--- 고양이와 개는 몇 마리 있을까
+-- 고양이와 개는 몇 마리 있을까 (Level 2)
 SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) AS count 
 FROM ANIMAL_INS
 GROUP BY ANIMAL_TYPE
 ORDER BY ANIMAL_TYPE;
 -----------------------------------------------
--- 동명 동물 수 찾기
+-- 동명 동물 수 찾기 (Level 2)
 SELECT NAME, COUNT(NAME) AS COUNT
 FROM ANIMAL_INS
 WHERE NAME IS NOT NULL -- NULL값이 아닌 것들만
@@ -34,11 +24,7 @@ GROUP BY NAME
 HAVING COUNT > 1 -- 두 번 이상 쓰인 이름과 해당 이름이 쓰인 횟수를 조회하는 것이므로
 ORDER BY NAME;
 ---------------------------------------------------------------------------------
--- 모든 레코드 조회하기
-SELECT * FROM ANIMAL_INS
-ORDER BY ANIMAL_ID;
----------------------------------------------------------------------------------
--- 루시와 엘라 찾기
+-- 루시와 엘라 찾기 (Level 2)
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
 WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
