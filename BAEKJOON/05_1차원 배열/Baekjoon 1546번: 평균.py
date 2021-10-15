ef3626221e8f1cd8131dc279df29c403168e6e26
Brandon Jung <jwcnet93@gmail.com>
2021-10-15 11:40:55 +0900

    Create Baekjoon 1546번: 평균.py

---
+++ BAEKJOON/05_1차원 배열/Baekjoon 1546번: 평균.py
@@ -0,0 +1,14 @@
+n = int(input())
+scores = []
+
+for score in map(int, input().split()):
+    scores.append(score)
+
+M = max(scores)    
+new_scores = []
+
+# 모든 기존 점수를 점수/M*100으로 고쳐서 새로운 점수 리스트에 추가
+for i in range(len(scores)):
+    new_scores.append(scores[i] / M * 100)
+
+print(sum(new_scores)/len(new_scores)) # 평균 계산
