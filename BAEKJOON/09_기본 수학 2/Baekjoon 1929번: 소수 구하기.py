dbb8f91ecb5b1a713c6e5bb674e22da58bd74bb2
Brandon Jung <jwcnet93@gmail.com>
2021-10-15 12:37:12 +0900

    Create Baekjoon 1929번: 소수 구하기.py

---
+++ BAEKJOON/09_기본 수학 2/Baekjoon 1929번: 소수 구하기.py
@@ -0,0 +1,21 @@
+import sys
+m, n = map(int, sys.stdin.readline().split())
+
+def isprime(m, n):
+# 에라토스테네스의 체
+    a = [False,False] + [True]*(n-1)
+    primes = []
+
+    for i in range(2,n+1):
+        if a[i]:
+            primes.append(i)
+            for j in range(2*i, n+1, i):
+                a[j] = False
+    
+    # 소수 출력
+    for prime in primes:
+        if prime >= m: # m 과 같거나 큰 값만...
+            print(prime)
+
+# 위에서 정의한 함수 호출
+isprime(m, n)
