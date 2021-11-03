349654651edc78975caf043cdad5855ad49ab604
Brandon Jung <jwcnet93@gmail.com>
2021-11-03 14:47:53 +0900

    Update Baekjoon 10809번: 알파벳 찾기.py

--- BAEKJOON/07_문자열/Baekjoon 10809번: 알파벳 찾기.py
+++ BAEKJOON/07_문자열/Baekjoon 10809번: 알파벳 찾기.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline()
 
 S = input
-alphabet = list(range(97, 123)) # 아스키코드 range
+alphabet = list(range(97, 123)) # 소문자들 아스키코드 range
 
 for i in alphabet:
     print(S.find(chr(i))) # chr() 아스키코드 → 문자
