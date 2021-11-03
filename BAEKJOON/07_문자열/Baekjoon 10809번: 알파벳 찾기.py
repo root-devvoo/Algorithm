93c35fa5e25cc7bb8326ec79c6c06a9a762ef76f
Brandon Jung <jwcnet93@gmail.com>
2021-11-03 14:47:17 +0900

    Create Baekjoon 10809번: 알파벳 찾기.py

---
+++ BAEKJOON/07_문자열/Baekjoon 10809번: 알파벳 찾기.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline()
+
+S = input
+alphabet = list(range(97, 123)) # 아스키코드 range
+
+for i in alphabet:
+    print(S.find(chr(i))) # chr() 아스키코드 → 문자
+    # find() 는 찾는 문자가 문자열 안에 포함되지 않은 경우 -1을 출력
