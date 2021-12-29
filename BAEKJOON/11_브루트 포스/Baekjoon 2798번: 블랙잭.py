2dbbab4ac9679182c78fe39016a0ca9d9c5c6bd5
Brandon Jung <jwcnet93@gmail.com>
2021-12-29 11:17:59 +0900

    Update Baekjoon 2798번: 블랙잭.py

--- BAEKJOON/11_브루트 포스/Baekjoon 2798번: 블랙잭.py
+++ BAEKJOON/11_브루트 포스/Baekjoon 2798번: 블랙잭.py
@@ -6,13 +6,12 @@ for i in range(len(cards)):
     for j in range(i+1, len(cards)):
         for k in range(j+1, len(cards)):
             if cards[i] + cards[j] + cards[k] > M: # 세 카드를 더한 값이 M보다 클(초과) 경우
-                continue
+                continue # 다시 인덱스 반복(변경)해서 덧셈 실행하도록...
             else: # 세 카드를 더한 값이 M보다 미만일 경우
                 total = max(total, (cards[i] + cards[j] + cards[k]))
                 # 정답의 가능성이 있으므로 total값과 비교해서 최대값 total에 덮어 저장
 
 print(total)
-
 '''
 3중 for문을 쓰면 '시간 초과' 같은게 뜰 줄 알았는데, 맞았다! lol
 다른 사람 풀이 중에서는 보다 효율적인 방법으로 푼 해답이 있을거라고 생각한다.
