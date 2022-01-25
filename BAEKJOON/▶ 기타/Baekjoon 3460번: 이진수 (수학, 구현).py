672443f7be74774f48acbb263a5f488fe6524cdb
Brandon Jung <jwcnet93@gmail.com>
2022-01-25 10:58:47 +0900

    Update Baekjoon 3460번: 이진수 (수학, 구현).py

--- BAEKJOON/▶ 기타/Baekjoon 3460번: 이진수 (수학, 구현).py
+++ BAEKJOON/▶ 기타/Baekjoon 3460번: 이진수 (수학, 구현).py
@@ -5,5 +5,5 @@ for _ in range(T):
     n = bin(int(input()))[2:] # 0b를 제외한 숫자만 사용하기 위해서 [2:] 사용
     # 최하위 비트(least significant bit, lsb)의 위치는 0
     for i, v in enumerate(n[::-1]): # 그러므로, 역순으로 탐색한다.
-        if v == '1':
+        if v == '1': # (bin 함수는 문자열로 반환한다)
             print(i, end=" ") # 1이 있는 인덱스 반환(출력)
