deabf0beb9a73d4e1aa82cb4a36080cb58a157fe
Brandon Jung <jwcnet93@gmail.com>
2022-07-07 10:46:17 +0900

    Create Baekjoon 4949번: 균형잡힌 세상.py

---
+++ BAEKJOON/18_스택/Baekjoon 4949번: 균형잡힌 세상.py
@@ -0,0 +1,33 @@
+while True:
+    str = input()
+    stack = []
+    
+    # 마침표가 나오면 break 걸어줌 
+    # (stack이 비어있는 상태에서 마침표로만 끝나도 yes로 간주)   
+    if str == '.':
+        break 
+    
+    # 괄호 판별
+    for c in str:
+        if c == '[' or c == '(':
+            stack.append(c)    
+        elif c == ']':
+            # 스택에 비어있지 않으면서 짝이 맞는 '[' 괄호가 있다면
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            # 스택에 조건에 맞는 괄호가 없는 경우
+            else:
+                stack.append(']')
+                break
+        elif c == ')':
+            # 스택에 비어있지 않으면서 짝이 맞는 '(' 괄호가 있다면
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            # 스택에 조건에 맞는 괄호가 없는 경우  
+            else:
+                stack.append(')')
+                break       
+    # 위 for문에서 break가 걸리지 않았다면
+    if len(stack) == 0:
+        print('yes')
+    else:
